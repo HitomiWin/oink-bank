3e8b012e527042aaf0941732f07a320385342b5e
HitomiWin <hitomi.winberg@gmail.com>
2021-12-29 07:57:47 +0100

    use normal firebase syntax to get document instead of react-query-firebase

--- client/src/pages/EditChild.tsx
+++ client/src/pages/EditChild.tsx
@@ -1,22 +1,17 @@
 import { memo, VFC } from "react";
 import { ChildEditForm } from "../components/ChildEditForm";
 import { useParams } from "react-router-dom";
-import { Alert } from "react-bootstrap";
+import useGetDocument from "../hooks/useGetDocument";
 
-import useGetChild from "../hooks/useGetChild";
 
 export const EditChild: VFC = memo(() => {
   const { id } = useParams();
-  const childQuery = useGetChild(id ?? "");
-
-  if (childQuery.isError) {
-    return <Alert variant="warning">{childQuery.error}</Alert>;
-  }
+  const childQuery = useGetDocument('children', id??"" );
 
   if (childQuery.isLoading) {
     return <p>Loading...</p>;
   }
-  return childQuery.isSuccess && childQuery.data ?(
+  return childQuery.data ?(
     <>
    <h3 className="text-center">Edit {childQuery.data.name} </h3>
       <ChildEditForm id={id} child={childQuery.data} />
